6f0e55c2b373be523e1afca51e77b0c17f6cf455
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-07 20:29:50 -0600

    feat: test many-to-one relationship between Invoice and Client on app startup

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,55 @@
 package com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Client;
+import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Invoice;
+import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.repositories.ClientRepository;
+import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.repositories.InvoiceRepository;
 
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClientRepository clientRepository;
+	@Autowired
+	private InvoiceRepository invoiceRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		manyToOneFindByIdClient(1L);
+	}
+
+	@Transactional
+	public void manyToOne() {
+		Client client = new Client("Santiago", "Díaz");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("Office supplies", 2000L);
+		invoice.setClient(client);
+		Invoice invoiceDB = invoiceRepository.save(invoice);
+		System.out.println(invoiceDB);
+	}
+
+	@Transactional
+	public void manyToOneFindByIdClient(Long id) {
+			clientRepository.findById(id).ifPresentOrElse(c -> {
+			Invoice invoice = new Invoice("Office supplies", 2000L);
+			invoice.setClient(c);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println(invoiceDB);
+		}, () -> System.out.println("Client not found by id " + id));
+
 	}
 
 }
