c641fb97116435cfaafa1d47cb976f612b9152c9
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-07 20:26:24 -0600

    feat: add initial client data in import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into clients (name, lastname) values('Alfonso','Vidrio');
+insert into clients (name, lastname) values('Karla','Balderrama');
+insert into clients (name, lastname) values('Jorge','Loza');
