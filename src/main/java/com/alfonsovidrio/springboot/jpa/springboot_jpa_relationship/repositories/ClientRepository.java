a005e9964b87b661d70f4edd5caff1ea8bbaf5de
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-22 00:55:42 -0600

    feat: add method to fetch client with addresses using JPQL

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
@@ -1,9 +1,13 @@
 package com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Client;
 
 public interface ClientRepository extends CrudRepository<Client, Long>{
-    
+    @Query("SELECT c FROM Client c LEFT JOIN FETCH c.addresses WHERE c.id = :id")
+    Optional<Client> findOne(Long id);
 }
