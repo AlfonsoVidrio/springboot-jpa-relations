06c121435fef3b96378e1452cbae2fb75c8b5873
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-21 23:12:27 -0600

    feat: implementation of one-to-many relationship between Client and Address

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,5 +1,6 @@
 package com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Address;
 import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Client;
 import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Invoice;
 import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.repositories.ClientRepository;
@@ -27,7 +29,58 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOneFindByIdClient(1L);
+		removeAddress();
+		// oneToManyFindById(2L);
+		// oneToMany();
+		// manyToOneFindByIdClient(1L);
+		// manyToOne();
+	}
+
+	@Transactional
+	public void removeAddress() {
+		Client client = new Client("Fran", "Moras");
+		Address address1 = new Address("El verjel", 1234);
+		Address address2 = new Address("Vasco de Gama", 4892);
+
+		client.getAddresses().add(address1);
+		client.getAddresses().add(address2);
+
+		clientRepository.save(client);
+		System.out.println(client);
+
+		Optional<Client> optionalClient = clientRepository.findById(4L);
+		optionalClient.ifPresent(c -> {
+			c.getAddresses().remove(address1);
+			clientRepository.save(c);
+			System.out.println("client e: " + c);
+		});
+	}
+
+	@Transactional
+	public void oneToManyFindById(Long id) {
+		Optional<Client> optionalClient = clientRepository.findById(id);
+		optionalClient.ifPresentOrElse(client -> {
+			Address address1 = new Address("El verjel", 1234);
+			Address address2 = new Address("Vasco de Gama", 4892);
+
+			client.setAddresses(Arrays.asList(address1, address2));
+
+			clientRepository.save(client);
+			System.out.println(client);
+		}, () -> System.out.println("Client not found by id " + id));
+	}
+
+	@Transactional
+	public void oneToMany() {
+		Client client = new Client("Fran", "Moras");
+		Address address1 = new Address("El verjel", 1234);
+		Address address2 = new Address("Vasco de Gama", 4892);
+
+		client.getAddresses().add(address1);
+		client.getAddresses().add(address2);
+
+		clientRepository.save(client);
+		System.out.println(client);
 	}
 
 	@Transactional
@@ -43,7 +96,7 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 	@Transactional
 	public void manyToOneFindByIdClient(Long id) {
-			clientRepository.findById(id).ifPresentOrElse(c -> {
+		clientRepository.findById(id).ifPresentOrElse(c -> {
 			Invoice invoice = new Invoice("Office supplies", 2000L);
 			invoice.setClient(c);
 			Invoice invoiceDB = invoiceRepository.save(invoice);
