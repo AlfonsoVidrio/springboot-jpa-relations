15666ae2cdec47b7166aff2ec9bd3f711cf9404f
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-21 23:11:35 -0600

    feat: add one-to-many relationship from Client to Address using a custom join table

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/entities/Client.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -1,10 +1,19 @@
 package com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "clients")
@@ -15,19 +24,30 @@ public class Client {
     private String name;
     private String lastname;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    // @JoinColumn(name = "client_id")
+
+    // custom intermediate table
+    @JoinTable(
+        name = "tbl_clients_to_addresses", 
+        joinColumns = @JoinColumn(name = "id_client"),
+        inverseJoinColumns = @JoinColumn(name = "id_addresses"),
+        uniqueConstraints = @UniqueConstraint(columnNames = {"id_addresses", "id_client"}))    
+    private List<Address> addresses;
+    
     public Client() {
+        addresses = new ArrayList<>();
     }
 
     public Client(String name, String lastname) {
+        this();
         this.name = name;
         this.lastname = lastname;
     }
     public Long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
+
     public String getName() {
         return name;
     }
@@ -40,9 +60,18 @@ public class Client {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+    
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
 
     @Override
     public String toString() {
-        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + "}";
+        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + "addresses=" + addresses +"}";
     }
 }
+
