70ed802805d3504fe624c538afa1cdad4e32dc89
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-23 01:07:59 -0600

    feat: add JPQL queries to fetch Client with addresses, invoices, or both using JOIN FETCH

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
@@ -9,5 +9,11 @@ import com.alfonsovidrio.springboot.jpa.springboot_jpa_relationship.entities.Cli
 
 public interface ClientRepository extends CrudRepository<Client, Long>{
     @Query("SELECT c FROM Client c LEFT JOIN FETCH c.addresses WHERE c.id = :id")
+    Optional<Client> findOneWithAddresses(Long id);
+
+    @Query("SELECT c FROM Client c LEFT JOIN FETCH c.invoices WHERE c.id = :id")
+    Optional<Client> findOneWithInvoices(Long id);
+
+    @Query("SELECT c FROM Client c LEFT JOIN FETCH c.invoices LEFT JOIN FETCH c.addresses WHERE c.id = :id")
     Optional<Client> findOne(Long id);
 }
