31446a3291c9dce98a7834f53811541f6207c018
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-24 23:54:33 -0600

    feat: include clientDetails in JOIN FETCH query for findOne in ClientRepository

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa_relationship/repositories/ClientRepository.java
@@ -14,6 +14,6 @@ public interface ClientRepository extends CrudRepository<Client, Long>{
     @Query("SELECT c FROM Client c LEFT JOIN FETCH c.invoices WHERE c.id = :id")
     Optional<Client> findOneWithInvoices(Long id);
 
-    @Query("SELECT c FROM Client c LEFT JOIN FETCH c.invoices LEFT JOIN FETCH c.addresses WHERE c.id = :id")
+    @Query("SELECT c FROM Client c LEFT JOIN FETCH c.invoices LEFT JOIN FETCH c.addresses LEFT JOIN FETCH c.clientDetails WHERE c.id = :id")
     Optional<Client> findOne(Long id);
 }
