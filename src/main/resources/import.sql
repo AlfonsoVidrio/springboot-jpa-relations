cbafb576185ddd7da1136519c5c2b44d80b13df1
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-04-26 00:35:48 -0600

    chore: add initial data for students and courses in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,16 @@
 insert into clients (name, lastname) values('Alfonso','Vidrio');
 insert into clients (name, lastname) values('Karla','Balderrama');
 insert into clients (name, lastname) values('Jorge','Loza');
+insert into clients (name, lastname) values('Javier','Lopez');
+insert into clients (name, lastname) values('Jorge','Gonzalez');
+insert into clients (name, lastname) values('Alberto','Diaz');
+
+insert students (name, lastname) values('Dillon','Harris');
+insert students (name, lastname) values('Liam','Smith');
+insert students (name, lastname) values('Emma','Johnson');
+insert students (name, lastname) values('Olivia','Williams');
+insert students (name, lastname) values('Ava','Brown');
+
+insert into courses (name, instructor) values('Java Fundamentals','Cisco Networking Academy');
+insert into courses (name, instructor) values('Python for Everybody','University of Michigan');
+insert into courses (name, instructor) values('Introduction to SQL','Coursera');
